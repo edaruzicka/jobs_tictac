8dda4b52cde23325ff04f4af55f74815cf6c5751
Eduard Ruzicka <edaruzicka@seznam.cz>
2022-02-04 22:05:25 +0100

    register

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import configparser
 config = configparser.ConfigParser()
 config.read('account.conf')
 acc_info = config['acc_info']
-email = acc_info['email']
+user_id = acc_info['user_id']
+user_token = acc_info['user_token']
 
-print(email)
+print(f"{user_id} {user_token}")
--- register.py
+++ register.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 import configparser
 
@@ -8,4 +9,9 @@ acc_info = config['acc_info']
 email = acc_info['email']
 nickname = acc_info['nickname']
 
-r = requests.post('https://piskvorky.jobs.cz/api/v1/user')
+data = {
+    'nickname':nickname, 
+    'email':email}
+
+r = requests.post('https://piskvorky.jobs.cz/api/v1/user', json=data)
+print(r.json())
