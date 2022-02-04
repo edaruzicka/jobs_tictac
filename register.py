cb228cd08e06dafaff144b0bd6a071c88e6cb0f8
Eduard Ruzicka <edaruzicka@seznam.cz>
2022-02-04 21:46:32 +0100

    idklol

--- register.py
+++ register.py
@@ -1,8 +1,11 @@
+import requests
+
 import configparser
 
 config = configparser.ConfigParser()
 config.read('account.conf')
 acc_info = config['acc_info']
 email = acc_info['email']
+nickname = acc_info['nickname']
 
-print(email)
+r = requests.post('https://piskvorky.jobs.cz/api/v1/user')
