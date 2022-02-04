26e16f88752b19fb44f5d65c9bf5965956652399
Eduard Ruzicka <edaruzicka@seznam.cz>
2022-02-04 23:21:45 +0100

    sleep

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import configparser
-from re import X
-from tkinter import Y
 import requests
+import configparser
 import json
+import time
 
 config = configparser.ConfigParser()
 config.read('account.conf')
@@ -35,23 +34,45 @@ def check_status(user_token, game_token):
     json_response = r.json()
     print(json_response)
 
-    return
+    actual_player_id = json_response['actualPlayerId']
+    winner_id = json_response['winnerId']
+
+    return actual_player_id, winner_id
 
 def check_last_status(user_token, game_token):
     r = requests.post('https://piskvorky.jobs.cz/api/v1/checkLastStatus', json = {'userToken':user_token, 'gameToken':game_token})
     json_response = r.json()
     print(json_response)
 
-    player_id = json_response['coordinates']['playerId']
-    x = json_response['coordinates']['x']
-    y = json_response['coordinates']['y']
+    actual_player_id = json_response['actualPlayerId']
+    #x = json_response['coordinates']['x']
+    #y = json_response['coordinates']['y']
 
-    return
+    return actual_player_id
 
 def main():
     print(f"{user_id} {user_token}")
 
     game_token, game_id = connect(user_token)
+    winner_id = None
+
+    while winner_id == None:
+        print('new run')
+        time.sleep(10)
+
+        actual_player_id, winner_id = check_status(user_token, game_token)
+
+        time.sleep(10)
+
+        if actual_player_id == user_token:
+            #play(user_token, game_token, 0, 0)
+            print('MY TURN')
+            player_id = check_last_status(user_token, game_token)
+        else:
+            print('waiting...')
+            player_id = check_last_status(user_token, game_token)
+            
+
 
 if __name__ == "__main__":
     main()
